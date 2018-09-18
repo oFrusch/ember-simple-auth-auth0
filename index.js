d4af18bedc9c61a67f205ceb4180893c2973a3e6
Xaser Acheron <xaser.88@gmail.com>
2018-09-18 17:31:21 -0500

    always set NODE_ENV to 'production' for webpack build

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = {
         },
         plugins: [
           new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(EmberAddon.env())
+            'process.env.NODE_ENV': 'production'
           })
         ]
       }
