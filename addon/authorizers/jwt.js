d1a23e2ebc246b26db8a263d58db82e65a96d9eb
Xaser Acheron <xaser.88@gmail.com>
2018-09-19 15:35:50 -0500

    added explicit deprecation warning to jwtAuthorizer

--- addon/authorizers/jwt.js
+++ addon/authorizers/jwt.js
@@ -1,9 +1,19 @@
 import { isPresent } from '@ember/utils';
 import { debug } from '@ember/debug';
+import { deprecate } from '@ember/application/deprecations';
 import BaseAuthorizer from 'ember-simple-auth/authorizers/base';
 
 export default BaseAuthorizer.extend({
   authorize(sessionData, block) {
+    deprecate(`Ember Simple Auth Auth0: Authorizers are deprecated in Ember Simple Auth. Consider using the 'authorize' method of the auth0 service instead.`,
+      false,
+      {
+        id: 'ember-simple-auth-auth0.jwtAuthorizer',
+        until: '5.0.0',
+        url: 'https://github.com/simplabs/ember-simple-auth#deprecation-of-authorizers',
+      }
+    );
+
     let userToken = sessionData['idToken'];
 
     if (isPresent(userToken)) {
