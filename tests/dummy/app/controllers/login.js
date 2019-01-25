0ca21936277f0586b4280f8ae331f1130c710042
Miguel Andrade <jmandrade0@gmail.com>
2019-01-25 16:10:23 +0000

    update dependencies

--- tests/dummy/app/controllers/login.js
+++ tests/dummy/app/controllers/login.js
@@ -2,11 +2,6 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 import { get } from '@ember/object';
-import Ember from 'ember';
-
-const {
-  Logger
-} = Ember;
 
 export default Controller.extend({
   session: service(),
@@ -23,7 +18,8 @@ export default Controller.extend({
       };
 
       get(this, 'session').authenticate('authenticator:auth0-lock-passwordless', lockOptions, () => {
-        Logger.info('Passwordless sent');
+        // eslint-disable-next-line no-console
+        console.log('Passwordless sent');
       });
     }
   }
--- tests/dummy/app/routes/protected.js
+++ tests/dummy/app/routes/protected.js
@@ -1,16 +1,12 @@
 import Route from '@ember/routing/route';
-import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
-const {
-  Logger
-} = Ember;
 
 export default Route.extend(AuthenticatedRouteMixin, {
   authenticationRoute: 'login',
   model() {
     return this.store.findAll('post').catch((err) => {
-      Logger.error(err);
+      // eslint-disable-next-line no-console
+      console.error(err);
     });
   }
 });
