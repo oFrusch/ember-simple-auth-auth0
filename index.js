f7f2abc03d5561aaf69c29517a3b5bf15fa08c2f
Miguel Andrade <jmandrade0@gmail.com>
2019-06-04 17:55:51 +0100

    make addon compatible with ember-auto-import 1.4.0

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ module.exports = {
     },
     autoImport:{
       webpack: {
-        performance: {
-          maxEntrypointSize: 1048576, // 1 MiB
-          maxAssetSize: 1048576
-        },
         plugins: [
           new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
           })
-        ]
+        ],
+        node: {
+          global: true
+        }
       }
     }
   }
